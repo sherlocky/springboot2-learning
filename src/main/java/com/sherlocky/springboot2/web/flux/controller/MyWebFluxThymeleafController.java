f755f83b7bf87ca0f47de068988e178ce38e1a2e
Sherlocky <y836097668@126.com>
2018-11-20 16:21:50 +0800

    测试thymeleaf模板返回页面

---
+++ src/main/java/com/sherlocky/springboot2/web/flux/controller/MyWebFluxThymeleafController.java
@@ -0,0 +1,62 @@
+package com.sherlocky.springboot2.web.flux.controller;
+
+import com.sherlocky.springboot2.domain.User;
+import com.sherlocky.springboot2.web.flux.handler.UserHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * WebFlux 中使用 Thymeleaf 模板语言返回页面
+ * @author: zhangcx
+ * @date: 2018/11/20 13:49
+ */
+// 注意不要使用@RestController注解,使用@RestController不能返回页面返回的是字符串(路径的字符串)。
+@Controller
+@RequestMapping("/flux")
+public class MyWebFluxThymeleafController {
+    private static final String USER_LIST_PATH_NAME = "userList";
+
+    /** WebFlux 中使用 Thymeleaf 模板语言返回页面 */
+    @Autowired
+    private UserHandler userHandler;
+
+    /**
+     * Thymeleaf 是现代的模板语言引擎，可以独立运行也可以服务于 Web，主要目标是为开发提供天然的模板，并且能在 HTML 里面准确的显示。是新一代 Java 模板引擎，在 Spring 4 后推荐使用。
+     *
+     * 常用语法糖如下：
+     * ${...}：变量表达式；
+     * th:text：处理 Tymeleaf 表达式；
+     * th:each：遍历表达式，可遍历的对象有，实现 java.util.Iterable、java.util.Map（遍历时取 java.util.Map.Entry）、array 等
+     */
+    // 返回值 Mono<String> 或者 String 都行，但是 Mono<String> 代表着 这个返回 View 也是回调的
+    @GetMapping("/hello")
+    public Mono<String> hello(final Model model) {
+        model.addAttribute("name", "我是DJ");
+        model.addAttribute("id", "123456");
+
+        String path = "hello";
+        return Mono.create(monoSink -> monoSink.success(path));
+    }
+
+    // Model 对象来进行数据绑定到视图
+    @GetMapping("/hello/list")
+    public String listPage(final Model model) {
+        final Flux<User> userFluxList = userHandler.listUsers();
+        // TODO 会报表达式错误（Exception evaluating SpringEL expression:）
+        /**
+         * 具体错误如下：
+         * org.springframework.expression.spel.SpelEvaluationException: EL1008E: Property or field 'id' cannot be found on object of
+         * type 'reactor.core.publisher.FluxIterable' - maybe not public or not valid?
+         */
+        // tomcat9 和 undertow2 均报同样的错
+        model.addAttribute("users", userFluxList);
+        // return 字符串，该字符串对应的目录在 resources/templates 下的模板名字
+        // 一般会集中用常量管理模板视图的路径
+        return USER_LIST_PATH_NAME;
+    }
+}
--- src/main/java/com/sherlocky/springboot2/web/mvc/MyMVCController.java
+++ src/main/java/com/sherlocky/springboot2/web/mvc/MyMVCController.java
@@ -1,7 +1,10 @@
 package com.sherlocky.springboot2.web.mvc;
 
-import com.sherlocky.springboot2.web.entity.User;
+import com.sherlocky.springboot2.dao.UserRepository;
+import com.sherlocky.springboot2.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +21,30 @@ import java.util.stream.Stream;
 @Controller
 @RequestMapping("/mvc/users")
 public class MyMVCController {
+    private static final String USER_LIST_PATH_NAME = "userList";
+    @Autowired
+    private UserRepository userRepository;
 
+    /** Springmvc 中使用 Thymeleaf 模板语言返回页面 */
+    @RequestMapping("/hello")
+    public String hello(final Model model) {
+        model.addAttribute("name", "我是DJ");
+        model.addAttribute("id", "123456");
+        return "hello";
+    }
+
+    // Model 对象来进行数据绑定到视图
+    @RequestMapping("/hello/list")
+    public String listPage(final Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        // return 字符串，该字符串对应的目录在 resources/templates 下的模板名字
+        // 一般会集中用常量管理模板视图的路径
+        return USER_LIST_PATH_NAME;
+    }
+
+
+
+    /** 使用 SpringMVC 实现 Restful 接口 */
     @RequestMapping("")
     @ResponseBody
     public List<User> listUsers() {
