abb3261c3dc20a23767b0b21e12f471742ca622a
Sherlocky <y836097668@126.com>
2018-11-21 15:46:43 +0800

    修正spring websocket无法连接问题

--- websocket/src/main/java/com/sherlocky/springboot2/websocket/web/config/MyWebSocketConfig.java
+++ websocket/src/main/java/com/sherlocky/springboot2/websocket/web/config/MyWebSocketConfig.java
@@ -9,12 +9,13 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 /**
- * @description webSocket配置类，绑定前端连接端点url及其他信息
+ * @description webSocket配置类，实现接口来配置Websocket请求的路径和拦截器及其他信息
  */
 @Configuration
 @EnableWebSocket
 public class MyWebSocketConfig implements WebSocketConfigurer {
-    private static String WEBSOCKET_URL = "/websocket";
+    // websocket请求路径
+    private static String WEBSOCKET_URL = "/websocket/echo";
     @Autowired
     private MyWebSocketHandler myWebSocketHandler;
     @Autowired
@@ -23,12 +24,9 @@ public class MyWebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // 绑定前端连接端点url
-        registry.addHandler(myWebSocketHandler, WEBSOCKET_URL).addInterceptors(myHandshakeInterceptor).withSockJS();
-        // withSockJS: 开启SockJS支持
+        registry.addHandler(myWebSocketHandler, WEBSOCKET_URL)
+                .setAllowedOrigins("*") // 必须添加
+                .addInterceptors(myHandshakeInterceptor);
+        // .withSockJS: 开启SockJS支持 (测试开启后，无法连接websocket，原因暂不明)
     }
-
-/*    @Bean
-    ServletWebServerFactory servletWebServerFactory(){
-        return new TomcatServletWebServerFactory();
-    }*/
 }
