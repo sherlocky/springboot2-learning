a0fc73aba29847808b595c70f5339d1a95f4ff43
Sherlocky <y836097668@126.com>
2018-11-21 16:11:17 +0800

    模拟的客户端用户id，改为自增的AtomicLong

--- websocket/src/main/java/com/sherlocky/springboot2/websocket/web/interceptor/MyHandshakeInterceptor.java
+++ websocket/src/main/java/com/sherlocky/springboot2/websocket/web/interceptor/MyHandshakeInterceptor.java
@@ -1,5 +1,6 @@
 package com.sherlocky.springboot2.websocket.web.interceptor;
 
+import com.sherlocky.springboot2.websocket.web.handler.MyWebSocketHandler;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.server.ServerHttpRequest;
@@ -11,16 +12,16 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 import java.util.Map;
 
 /**
- * @description 前端页面与后台通信握手拦截器, 可用于完善定向发送信息功能。
+ * 前端页面与后台通信握手拦截器, 可用于完善定向发送信息功能。
  */
 @Component
 public class MyHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
     private static Log log = LogFactory.getLog(HttpSessionHandshakeInterceptor.class);
 
     @Override
-    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,Map<String, Object> attributes) throws Exception {
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,Map<String, Object> attributes) {
         // 此处可将用户信息放入WebSocketSession的属性当中，以便定向发送消息。
-        attributes.put("WEBSOCKET_USERID", 1L);
+        attributes.put("WEBSOCKET_USERID", MyWebSocketHandler.createWebSocketClientUserId());
         return true;
     }
 
