17d0345db885aeed8610004bae8ebd9bf075b873
Sherlocky <y836097668@126.com>
2018-12-02 17:58:19 +0800

    添加springboot2-mybatis-generator示例

---
+++ mybatis-generator/src/main/java/com/sherlocky/springboot2/mybatis/generator/Springboot2MybatisGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.sherlocky.springboot2.mybatis.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MybatisGeneratorApplication.class, args);
+    }
+}
