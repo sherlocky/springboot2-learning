24bddef72c678b97554f16eca204b957779214d8
Sherlocky <y836097668@126.com>
2018-12-13 13:50:12 +0800

    MvcMock时支持REST doc生成

--- webflux/src/test/java/com/sherlocky/springboot2/Springboot2ApplicationWebfluxTests.java
+++ webflux/src/test/java/com/sherlocky/springboot2/Springboot2ApplicationWebfluxTests.java
@@ -5,17 +5,20 @@ import com.sherlocky.springboot2.web.mvc.MyMVCController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MyMVCController.class)
+@AutoConfigureRestDocs // 会自动在 build\generated-snippets\ 下生成 API 文档
 public class Springboot2ApplicationWebfluxTests {
     //mvc 环境对象
     @Autowired
@@ -30,10 +33,23 @@ public class Springboot2ApplicationWebfluxTests {
         // perform：执行一个RequestBuilder请求，会自动执行SpringMVC的流程并映射到相应的控制器执行处理；
         String responseContext = this.mvc.perform(get("/mvc/users").contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk()) // andExpect：添加ResultMatcher验证规则，验证控制器执行完成后结果是否正确；
+                .andDo(document("list-users"))
                 // andDo：添加ResultHandler结果处理器，比如调试时打印结果到控制台；
                 .andReturn().getResponse().getContentAsString()
                 // andReturn：最后返回相应的MvcResult；然后进行自定义验证/进行下一步的异步处理；
         ;
+        System.out.println(responseContext);
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        String responseContext = this.mvc.perform(get("/mvc/users/1").contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()) // andExpect：添加ResultMatcher验证规则，验证控制器执行完成后结果是否正确；
+                .andDo(document("get-user"))
+                // andDo：添加ResultHandler结果处理器，比如调试时打印结果到控制台；
+                .andReturn().getResponse().getContentAsString()
+                // andReturn：最后返回相应的MvcResult；然后进行自定义验证/进行下一步的异步处理；
+                ;
         System.out.println(responseContext);
     }
 }
