b75766e1c8e26ffb5d12b09ece05a25545a9ca27
Sherlocky <y836097668@126.com>
2018-12-13 16:48:07 +0800

    添加注释

--- mybatis-generator/src/main/java/com/sherlocky/springboot2/mybatis/generator/Springboot2MybatisGeneratorApplication.java
+++ mybatis-generator/src/main/java/com/sherlocky/springboot2/mybatis/generator/Springboot2MybatisGeneratorApplication.java
@@ -3,6 +3,9 @@ package com.sherlocky.springboot2.mybatis.generator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 借助idea实现mybatis逆向工程
+ */
 @SpringBootApplication
 public class Springboot2MybatisGeneratorApplication {
 
