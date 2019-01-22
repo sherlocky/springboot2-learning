47e340efc715337843aec1be19b258283ce515f7
Sherlocky <y836097668@126.com>
2019-01-22 11:27:04 +0800

    开启 Spring 注解驱动的缓存管理功能

--- redis/src/main/java/com/sherlocky/springboot2/redis/Springboot2RedisApplication.java
+++ redis/src/main/java/com/sherlocky/springboot2/redis/Springboot2RedisApplication.java
@@ -2,12 +2,13 @@ package com.sherlocky.springboot2.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 简单redis使用示例
  */
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class Springboot2RedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(Springboot2RedisApplication.class, args);
