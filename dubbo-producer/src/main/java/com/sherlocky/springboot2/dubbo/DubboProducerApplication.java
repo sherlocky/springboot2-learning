a9b53822c637a9bfc996eca5828f07bc3ac4df28
Sherlocky <y836097668@126.com>
2019-07-18 14:22:36 +0800

    去掉@EnableDubbo（未了解具体用法）

--- dubbo-producer/src/main/java/com/sherlocky/springboot2/dubbo/DubboProducerApplication.java
+++ dubbo-producer/src/main/java/com/sherlocky/springboot2/dubbo/DubboProducerApplication.java
@@ -1,14 +1,15 @@
 package com.sherlocky.springboot2.dubbo;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Dubbo 服务端Provider 示例
+ * <p>可参考：<a href="http://dubbo.apache.org/zh-cn/docs/user/recommend.html">Dubbo文档-推荐用法</a></p>
+ *
  * @author: zhangcx
  * @date: 2019/7/15 14:58
  */
-@EnableDubbo(scanBasePackages  = "com.sherlocky.springboot2.dubbo.service")
 @SpringBootApplication
 public class DubboProducerApplication {
 
