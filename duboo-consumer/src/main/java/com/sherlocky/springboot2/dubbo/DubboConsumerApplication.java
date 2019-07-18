6ea4f1d931831a2e1b587afaf09eb15391cbf653
Sherlocky <y836097668@126.com>
2019-07-18 14:21:45 +0800

    去掉@EnableDubbo（未了解具体用法）

--- duboo-consumer/src/main/java/com/sherlocky/springboot2/dubbo/DubboConsumerApplication.java
+++ duboo-consumer/src/main/java/com/sherlocky/springboot2/dubbo/DubboConsumerApplication.java
@@ -3,7 +3,6 @@ package com.sherlocky.springboot2.dubbo;
 import com.sherlocky.springboot2.dubbo.service.BaseService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2019/7/15 14:58
  */
 @Slf4j
-// 消费端此处不声明 @EnableDubbo 也可以使用，暂时不解？
-@EnableDubbo
 @SpringBootApplication
 public class DubboConsumerApplication implements CommandLineRunner {
     @Reference(version = "${base.service.version}")
