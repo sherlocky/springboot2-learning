108ce67b048c81644d6dd79dff5597fc7bf0e61d
Sherlocky <y836097668@126.com>
2019-07-19 15:49:47 +0800

    改回使用zookeeper注册中心

--- dubbo-producer/src/main/java/com/sherlocky/springboot2/dubbo/DubboProducerApplication.java
+++ dubbo-producer/src/main/java/com/sherlocky/springboot2/dubbo/DubboProducerApplication.java
@@ -1,7 +1,15 @@
 package com.sherlocky.springboot2.dubbo;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.ConfigCenterConfig;
+import org.apache.dubbo.config.MetadataReportConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
 
 /**
  * Dubbo 服务端Provider 示例
@@ -10,10 +18,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: zhangcx
  * @date: 2019/7/15 14:58
  */
+@Slf4j
 @SpringBootApplication
-public class DubboProducerApplication {
+public class DubboProducerApplication implements CommandLineRunner {
+    @Resource
+    RegistryConfig registryConfig;
+    @Resource
+    MetadataReportConfig metadataReportConfig;
+    @Resource
+    ConfigCenterConfig configCenterConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(DubboProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("#####################################");
+        log.info("############# 注册中心");
+        log.info(JSON.toJSONString(registryConfig));
+        log.info("############# 元数据中心");
+        log.info(JSON.toJSONString(metadataReportConfig));
+        log.info("############# 配置中心");
+        log.info(JSON.toJSONString(configCenterConfig));
+        log.info("#####################################");
     }
 }
