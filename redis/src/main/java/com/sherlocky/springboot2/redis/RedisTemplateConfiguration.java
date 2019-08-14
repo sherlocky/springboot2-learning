04ce9754a887a3d9b276f7c3dbbdb61e455f38e4
Sherlocky <y836097668@126.com>
2019-08-14 12:33:51 +0800

    GenericFastJsonRedisSerializer VS FastJsonRedisSerializer

--- redis/src/main/java/com/sherlocky/springboot2/redis/RedisTemplateConfiguration.java
+++ redis/src/main/java/com/sherlocky/springboot2/redis/RedisTemplateConfiguration.java
@@ -1,6 +1,7 @@
 package com.app.document.config;
 
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -27,11 +28,12 @@ public class RedisTemplateConfiguration {
         template.setConnectionFactory(redisConnectionFactory);
 
         /**
-         * 通常使用 GenericFastJsonRedisSerializer 即可满足大部分场景，
+         * 通常使用 GenericFastJsonRedisSerializer 即可满足大部分场景，序列化时会将对象类型写入@type属性（要求对象必须实现无参构造器）
          * 如果你想定义特定类型专用的 RedisTemplate 可以使用 FastJsonRedisSerializer 来代替 GenericFastJsonRedisSerializer，配置是类似的。
-         * GenericFastJsonRedisSerializer fastJsonRedisSerializer = new GenericFastJsonRedisSerializer();
+         * 使用FastJsonRedisSerializer反序列化时，需要手动转换对象，否则会报ClassCastException
+         * FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
          */
-        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        GenericFastJsonRedisSerializer fastJsonRedisSerializer = new GenericFastJsonRedisSerializer();
 
         /** 设置默认的Serialize，包含 keySerializer & valueSerializer */
         template.setDefaultSerializer(fastJsonRedisSerializer);
